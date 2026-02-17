e903aaba47f407422d60bd64542d8b8ee93dddc7
Nicolas <nduranrodriguez@safareyes.es>
2026-02-17 16:44:11 +0100

    Modificaciones en el apartado del admin

--- src/Controller/Admin/RankingCrudController.php
+++ src/Controller/Admin/RankingCrudController.php
@@ -4,9 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\Ranking;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class RankingCrudController extends AbstractCrudController
 {
@@ -14,15 +11,4 @@ class RankingCrudController extends AbstractCrudController
     {
         return Ranking::class;
     }
-
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -14,15 +11,4 @@ class UserCrudController extends AbstractCrudController
     {
         return User::class;
     }
-
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
 }
